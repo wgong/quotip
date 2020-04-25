428d10a957193e842b2f48653e72bf4a5635ac8c
wgong <wen.g.gong@gmail.com>
2020-04-25 17:05:58 -0400

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-httpclient-example';
+
+
+  response2: any;
+
+  constructor(private http: HttpClient) {
+    console.log("calling httpClient");
+  }
+
+  ngOnInit() {
+
+  }
+
+  quote(){
+    this.http.get("https://cors-anywhere.herokuapp.com/https://av91vwnh7c.execute-api.us-east-1.amazonaws.com/v1/quotip")
+    .subscribe((response) => {
+      console.log(response);
+      this.response2 = response;
+    })
+  }
+
+}
