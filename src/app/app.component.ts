0f1d794a603f633c51ed09286572780f05d702aa
wgong <wen.g.gong@gmail.com>
2020-04-25 17:39:15 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,11 @@ import {HttpClient} from '@angular/common/http';
 export class AppComponent {
   title = 'quote-and-tip';
 
-
+  cors_utl = "https://cors-anywhere.herokuapp.com/https://av91vwnh7c.execute-api.us-east-1.amazonaws.com/v1/quotip";
   response2: any;
 
   constructor(private http: HttpClient) {
-    console.log("calling httpClient");
+    //console.log("calling httpClient");
   }
 
   ngOnInit() {
@@ -22,9 +22,9 @@ export class AppComponent {
   }
 
   quote(){
-    this.http.get("https://cors-anywhere.herokuapp.com/https://av91vwnh7c.execute-api.us-east-1.amazonaws.com/v1/quotip")
+    this.http.get(this.cors_utl)
     .subscribe((response) => {
-      console.log(response);
+      //console.log(response);
       this.response2 = response;
     })
   }
